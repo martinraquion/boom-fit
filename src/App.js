3678a449e5aeba0d8e7572be38d88d586b58cb68
Martin Earl Raquion <martin.raquion@boom.camp>
2020-04-24 02:36:21 +0800

    added prompt warning on mobile devices

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-
 import React from "react";
 import "./App.css";
 
 import StateProvider from "./context/stateContext";
 import BoomFitDashboard from "./components/BoomFitDashboard";
-
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { useTheme } from "@material-ui/core/styles";
+import Warning from "@material-ui/icons/Warning";
 function App() {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up("md"));
 
   return (
     <StateProvider>
@@ -15,10 +18,29 @@ function App() {
           style={{
             background:
               "no-repeat center url('https://images.pexels.com/photos/2098428/pexels-photo-2098428.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')",
-            backgroundSize: "cover"
+            backgroundSize: "cover",
           }}
         >
-          <BoomFitDashboard />
+          {matches ? (
+            <BoomFitDashboard />
+          ) : (
+            <div
+              style={{
+                fontSize: 16,
+                width: "70vw",
+                backgroundColor: "rgb(128, 128, 128, 0.3)",
+                padding: 20,
+                lineHeight: 1.4,
+              }}
+            >
+              <Warning />
+              <br />
+              This is a browser extension app. <br /> Preview is not available
+              on mobile.
+              <br />
+              Try it on Desktop
+            </div>
+          )}
         </header>
       </div>
     </StateProvider>
